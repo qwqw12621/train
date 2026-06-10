ec43c58b05accb8c03ecf19b319e386f960457fb
qwqw12621 <w9917327@gmail.com>
2026-06-11 07:31:44 +0800

    Update main.py

--- algorithm0610/main.py
+++ algorithm0610/main.py
@@ -1,2 +1,17 @@
-jovyan@vm-j4wlboxamd59:~$ ./run_test.sh greedy_opt.py D0000000.in 8 D0000000.out
-bash: ./run_test.sh: Permission denied
+jovyan@vm-j4wlboxamd59:~$ python verify.py greedy_opt.py D0000000.in 8 D0000000.out 
+Traceback (most recent call last):
+  File "/home/jovyan/verify.py", line 46, in <module>
+    main()
+  File "/home/jovyan/verify.py", line 7, in main
+    d = open(infile, 'rb').read().split(); N = int(d[0]); T = int(d[1]); pos = 2
+                                               ^^^^^^^^^
+ValueError: invalid literal for int() with base 10: b'import'
+jovyan@vm-j4wlboxamd59:~$ python verify.py greedy_opt.py D0000000_tight.in 8 D0000000_tight.out 
+Traceback (most recent call last):
+  File "/home/jovyan/verify.py", line 46, in <module>
+    main()
+  File "/home/jovyan/verify.py", line 7, in main
+    d = open(infile, 'rb').read().split(); N = int(d[0]); T = int(d[1]); pos = 2
+                                               ^^^^^^^^^
+ValueError: invalid literal for int() with base 10: b'import'
+jovyan@vm-j4wlboxamd59:~$ 
